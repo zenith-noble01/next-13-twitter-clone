4ff74869327a85ebf4a9b063c451c1f905b59da0
zenith-noble01 <webdev686@gmail.com>
2023-05-28 18:05:37 +0100

    want to start working on the main section of the page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,14 +8,14 @@ const Sidebar = () => {
   return (
     <div className="w-3/12 border-r min-h-screen py-4">
       <div className="flex flex-col gap-8">
-        <div className="icon h-[50px] w-[50px] rounded-full flex items-center justify-center  hover:bg-blue-500 cursor-pointer">
+        <div className="icon h-[50px] transition-all w-[50px] rounded-full flex items-center justify-center  hover:bg-blue-500 cursor-pointer">
           <FaTwitter className="text-3xl text-blue-500" />
         </div>
         <ul className="sidebar__routes">
           {sidebarRoutes.map((route, index) => (
             <Link href={route.path} key={index}>
               <li className="">
-                <route.icon className=" text-2xl" /> <span>{route.name}</span>{" "}
+                <route.icon className=" text-2xl" /> <span>{route.name}</span>
               </li>
             </Link>
           ))}
