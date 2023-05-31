0eaad1fe1f9b8a5b2d1132e47b2f8d1242e37ddd
zenith-noble01 <webdev686@gmail.com>
2023-05-31 19:01:40 +0100

    created the user model and the post model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import { model, Schema } from "mongoose"
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+const User = model('user', userSchema)
+
+export default User
